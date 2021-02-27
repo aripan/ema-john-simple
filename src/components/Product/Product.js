a9cf6e5e7f3cdef0e2962492de6f34aac70b11ae
aripan <aripan017@gmail.com>
2021-02-27 21:25:54 +0100

    Added eventHandler

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -4,7 +4,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
 
 const Product = (props) => {
-  //   console.log(props.product);
+  //   console.log(props);
   const { img, name, seller, price, stock } = props.product;
 
   return (
@@ -21,7 +21,12 @@ const Product = (props) => {
         <p>
           <small>Only {stock} left in stock - Order soon</small>
         </p>
-        <button className="main-button">
+        <button
+          className="main-button"
+          onClick={() => {
+            props.handleAddProduct(props.product.name);
+          }}
+        >
           <FontAwesomeIcon icon={faShoppingCart} />
           add to cart
         </button>
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -7,11 +7,15 @@ const Shop = () => {
   const first10 = fakeData.slice(0, 10);
   const [products, setProducts] = useState(first10);
 
+  const handleAddProduct = (productName) => {
+    console.log("Product Added in the list", productName);
+  };
+
   return (
     <div className="shop-container">
       <div className="product-container">
         {products.map((pd) => (
-          <Product product={pd} />
+          <Product product={pd} handleAddProduct={handleAddProduct} />
         ))}
       </div>
       <div className="cart-container">
